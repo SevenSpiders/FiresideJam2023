a1964de8d0f593350a0d4ce86568d2b46e3ca2bf
Decipherss <121757429+Decipherss@users.noreply.github.com>
2023-05-06 01:49:05 +0200

    Prototype

---
+++ Unity_FireSide2023/Assets/_Scripts/Marc Scripts/MA_Vortex.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MA_Vortex : MonoBehaviour
+{
+    public float pullForce = 10;
+    public float radius = 20;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other == null)
+            return;
+
+        Vector3 dir = (transform.position - other.transform.position).normalized;
+        float dist = Vector3.Distance(transform.position, other.transform.position);
+        float mul = 1-Mathf.Abs(dist / radius);
+        int inEye = dist < 1.5f ? 0 : 1;
+        Vector3 finalForce = (dir * pullForce * mul * inEye) / Time.deltaTime;
+        other.attachedRigidbody.AddForce(finalForce);
+    }
+}
