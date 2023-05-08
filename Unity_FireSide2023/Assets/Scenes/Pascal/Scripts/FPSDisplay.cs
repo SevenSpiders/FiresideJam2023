fd5ad62d84d412b7f4bf8ef5eec1820a93bbeb6d
Naramo <62239403+SevenSpiders@users.noreply.github.com>
2023-05-08 13:17:29 +0200

    implemented ocean tiling

---
+++ Unity_FireSide2023/Assets/Scenes/Pascal/Scripts/FPSDisplay.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+
+namespace Pascal {
+
+        
+    public class FPSDisplay : MonoBehaviour
+    {
+        public int avgFrameRate;
+        public TMP_Text display_Text;
+        
+        public void Update ()
+        {
+            float current = 0;
+            current = Time.frameCount / Time.time;
+            avgFrameRate = (int)current;
+            display_Text.text = avgFrameRate.ToString() + " FPS";
+        }
+    }
+
+}
